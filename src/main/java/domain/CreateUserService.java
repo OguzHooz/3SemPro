24ff19f5c745fe0bd983a0dc28f274f1ad769ec5
Alexander <alngu18@student.sdu.dk>
2021-11-29 15:34:28 +0100

    Login

---
+++ src/main/java/domain/CreateUserService.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class CreateUserService {
+
+    public CreateUserService() {
+
+    }
+
+    //Den her klasse henter data fra GUI og sender til databasen når der skal oprettes en bruger.
+
+}
