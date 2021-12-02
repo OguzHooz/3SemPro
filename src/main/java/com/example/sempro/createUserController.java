b00572ca8b0a17d1b328f6b7ef1a0be67f43018c
ngPsych <43138865+ngPsych@users.noreply.github.com>
2021-12-02 16:02:54 +0100

    New tab + create user fxml

---
+++ src/main/java/com/example/sempro/createUserController.java
@@ -0,0 +1,67 @@
+package com.example.sempro;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class createUserController {
+
+    @FXML
+    private TextField usernameTxtField;
+
+    @FXML
+    private PasswordField passwordTxtField;
+
+    @FXML
+    private PasswordField confirmPasswordTxtField;
+
+    @FXML
+    private TextField emailTxtField;
+
+    @FXML
+    private RadioButton managerRadioBtn;
+
+    @FXML
+    private RadioButton workerRadioBtn;
+
+    @FXML
+    private RadioButton guestRadioBtn;
+
+    @FXML
+    private Label alertLbl;
+
+    @FXML
+    private Button confirmBtn;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    void cancelOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guestRB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void managerRB(ActionEvent event) {
+
+    }
+
+    @FXML
+    void workerRB(ActionEvent event) {
+
+    }
+
+}
