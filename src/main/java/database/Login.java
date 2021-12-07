4a28684794838dce70020f240a7a3814b528da35
Farhiya11 <ffarhiyaa@gmail.com>
2021-12-07 13:01:51 +0100

    .

---
+++ src/main/java/database/Login.java
@@ -0,0 +1,49 @@
+package database;
+
+import domain.Encrypt;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+
+
+public class Login {
+     Encrypt encrypt;
+    DatabaseConnection connect;
+    // DatabaseConnection connect = new DatabaseConnection();
+   // Connection connectdatabase = connect.getConnection();
+
+    public  Login(){
+        encrypt=new Encrypt();
+        this.connect=new DatabaseConnection();
+
+    }
+
+    public boolean checkUser(String username, String password) {
+
+        boolean check = false;
+
+        try {
+            String sql = "SELECT username,password FROM user_info WHERE username = '" + username + "' AND password = '" + encrypt.encrypt(password) + "'";
+
+            Statement st = connect.getConnection().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            while(rs.next()) {
+                check = true;
+                System.out.println("RS.next(): " + check);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Login: " + check);
+        return check;
+    }
+
+
+}
+
+
+
+
